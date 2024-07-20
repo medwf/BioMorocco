d583d171fc74b380dc16371262cebaa4d9709596
med.wf <med.wf95@gmail.com>
2024-07-20 15:46:25 +0100

    Update email notifications to send separate emails for each store in an order

--- BackEnd/api/v1/views/order.py
+++ BackEnd/api/v1/views/order.py
@@ -26,76 +26,85 @@ def createOrders(product_id=None):
     from models.category import Category
     from models.store import Store
     from models.user import User
+
     data = request.get_json(force=True, silent=True)
     if not data:
         return jsonify({"error": "check your data send!"}), 400
 
-    message = []
-    error = []
-
     # selected one product directly
     if product_id:
         product = storage.get(Product, product_id)
+        if not product:
+            return jsonify({'error': f'Product id={product_id} Not Found!'}), 404
         quantity = data['quantity']
         total_price = (product.price * quantity) if request.user.country == 'Morocco' else (
             product.price * quantity) + 100
         products = [(product, quantity, total_price)]
 
-    # selected product from cart.
-    elif request.user and request.user.cart:
+    elif 'product_ids' in data:
         products = []
-        if 'product_ids' in data:
-            # no product found
-            if not len(data['product_ids']):
-                return jsonify({'error': 'Your cart is Empty!'}), 404
+        if not len(data['product_ids']):
+            return jsonify({'error': 'Your cart is Empty!'}), 404
 
-            # selected products
-            elif 'product_ids' in data:
-                for productId in data['product_ids']:
-                    for cartItem in request.user.cart.cartItems:
-                        if cartItem.product_id == productId:
-                            product = storage.get(Product, cartItem.product_id)
-                            quantity = cartItem.quantity
-                            total_price = (product.price * quantity) if request.user.country == 'Morocco' else (
-                                product.price * quantity) + 100
-                            products.append((product, quantity, total_price))
+        for productId in data['product_ids']:
+            for cartItem in request.user.cart.cartItems:
+                if cartItem.product_id == productId:
+                    product = storage.get(Product, cartItem.product_id)
+                    if not product:
+                        return jsonify({'error': 'Product Not Found!'}), 404
+
+                    quantity = cartItem.quantity
+                    total_price = (product.price * quantity) if request.user.country == 'Morocco' else (
+                        product.price * quantity) + 100
+                    products.append((product, quantity, total_price))
+    else:
+        return jsonify({'error': 'Product Not Found!'}), 404
+
+    if all((product.stock - quantity) < 0 for (
+        product, quantity, total_price) in products
+    ):
+        return jsonify({"error": "The stock of this product not available for Now!"}), 400
+
+    SEND_EMAIL_For_Each_Store = {}
 
     for (product, quantity, total_price) in products:
-        if product and product.stock and (product.stock - quantity) > 0:
 
-            order = Order(
-                status="pending",
-                quantity=quantity,
-                total_price=total_price,
-                orderValid=0,
-                user_id=request.user.id,
-                product_id=product.id
+        order = Order(
+            status="pending",
+            quantity=quantity,
+            total_price=total_price,
+            orderValid=0,
+            user_id=request.user.id,
+            product_id=product.id
+        )
+        storage.update(product, stock=(product.stock - order.quantity))
+        storage.new(order)
+        storage.save()
+        category = storage.get(Category, product.category_id)
+        store = storage.get(Store, category.store_id)
+        owner = storage.get(User, store.user_id)
+
+        # sending msg tell then that your store product is last then reminder stock.
+        if product.stock < product.reminder_stock:
+            content = Email.create_content_for_low_stock_dedicated(
+                owner, product
             )
-            storage.update(product, stock=(product.stock - order.quantity))
-            storage.new(order)
-            storage.save()
-            category = storage.get(Category, product.category_id)
-            store = storage.get(Store, category.store_id)
-            owner = storage.get(User, store.user_id)
+            Email.sendEmail(owner.email, content)
 
-            # sending msg tell then that your store product is last then reminder stock.
-            if product.stock < product.reminder_stock:
-                content = Email.create_content_for_low_stock_dedicated(
-                    owner, product
-                )
-                Email.sendEmail(owner.email, content)
-
-            message.append(
-                f"Order in product id: {product.id}, name: {product.name} created Successfully")
+        if owner.id not in SEND_EMAIL_For_Each_Store.keys():
+            SEND_EMAIL_For_Each_Store[owner.id] = [
+                (product, quantity, total_price)
+            ]
         else:
-
-            # sending msg to user store tell them he have an order but it is not found check your stock.
-            error.append(
-                f"Order in product id: {product.id}, name: {product.name} Not Found or not enough!"
+            SEND_EMAIL_For_Each_Store[owner.id].append(
+                (product, quantity, total_price)
             )
+
     # sending msg to user store tell them he have an order by sending name and phone and address.
-    content = Email.create_content_for_new_order(
-        owner, products, request.user
-    )
-    Email.sendEmail(owner.email, content)
-    return jsonify({"response": {"message": [msg for msg in message], "error": [err for err in error]}}), 200
+    for id, prds in SEND_EMAIL_For_Each_Store.items():
+        own = storage.get(User, id)
+        content = Email.create_content_for_new_order(
+            own, prds, request.user
+        )
+        Email.sendEmail(own.email, content)
+    return jsonify({"message": "Order created successfully"}), 200
